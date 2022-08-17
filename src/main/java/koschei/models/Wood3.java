1319fe893409570ad055b8b257b0226dd9bcd364
mvkola <106335750+mvkola@users.noreply.github.com>
2022-08-17 15:28:35 +0300

    This is the solution to task 2.1.4
    Pre-project

--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -3,9 +3,10 @@ package koschei.models;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+// Wood3 связано с Rabbit4 через конструктор с аннотацией @Autowired
+
 @Component
 public class Wood3 {
-
     private final Rabbit4 rabbit;
 
     @Autowired
