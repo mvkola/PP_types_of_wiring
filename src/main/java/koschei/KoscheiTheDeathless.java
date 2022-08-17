e73b0cb3e1f34d907eb82eab2ce938e587cb6c98
mvkola <106335750+mvkola@users.noreply.github.com>
2022-08-17 15:25:38 +0300

    This is the solution to task 2.1.4
    Pre-project

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -4,17 +4,19 @@ import koschei.models.Ocean1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+// KoscheiTheDeathless связан с Ocean1 через сеттер setOcean и аннотацию @Autowired над методом.
+// Аналогично свяжем Rabbit4 с Duck5.
+
 @Component
 public class KoscheiTheDeathless {
-
     private Ocean1 ocean;
-
-    public String getRulesByDeth() {
-        return "На свете есть океан , " + ocean.toString();
-    }
 
     @Autowired
     public void setOcean(Ocean1 ocean) {
         this.ocean = ocean;
+    }
+
+    public String getRulesByDeth() {
+        return "На свете есть океан , " + ocean.toString();
     }
 }
