97fd9707ba1dca11b20e43d637f48c936ca985a7
mvkola <106335750+mvkola@users.noreply.github.com>
2022-08-17 15:32:40 +0300

    This is the solution to task 2.1.4
    Pre-project

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,18 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+// Needle7 связано с Deth8 через через поле и аннотацию @Autowired
+
+@Component
 public class Needle7 {
+    
+    @Autowired
+    private Deth8 death;
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле " + death.toString();
     }
 }
