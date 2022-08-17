1e7d299c0e5705f992cecfb1ddfe7f3110da41a6
mvkola <106335750+mvkola@users.noreply.github.com>
2022-08-17 15:35:52 +0300

    This is the solution to task 2.1.4
    Pre-project

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /*
-Ocean1 связан с Island2 через поле и аннотацию, которая подтянет бин Island2 через метод getIsland()
-в классе AppConfig. Метод обозначен аннотацией @Bean, что автоматически подтягивает аргументы в метод.
+Ocean1 связан с Island2 через поле и аннотацию, которая подтянет бин Island2 через метод getIsland() в классе AppConfig. 
+Метод обозначен аннотацией @Bean, что автоматически подтягивает аргументы в метод.
 В качестве аргумента выступает бин Wood3. Аналогично свяжем Duck5 и Egg6.
 */
 
