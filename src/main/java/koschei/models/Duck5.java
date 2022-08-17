2f0f3308fc232b1ff33a1c70c510aacbd38d83e1
mvkola <106335750+mvkola@users.noreply.github.com>
2022-08-17 15:30:41 +0300

    This is the solution to task 2.1.4
    Pre-project

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,21 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+/*
+Duck5 связана с Egg6 через поле и аннотацию, которая подтянет бин Egg6 через метод getEgg()
+в классе AppConfig. Метод обозначен аннотацией @Bean, что автоматически подтягивает аргументы в метод.
+В качестве аргумента выступает бин Egg6. По аналогии Ocean1 - Island2.
+*/
 
 @Component
 public class Duck5 {
+    @Autowired
+    private Egg6 egg;
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
